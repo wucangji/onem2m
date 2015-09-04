73f1fbad39b35ab85d62c3c96c19f8771aaf15e8
samrg472 <samrg472@gmail.com>
2015-09-04 09:14:35 -0700

    Clean up

--- src/main/java/org/dsa/iot/onem2m/server/OneM2MServer.java
+++ src/main/java/org/dsa/iot/onem2m/server/OneM2MServer.java
@@ -2,24 +2,19 @@ package org.dsa.iot.onem2m.server;
 
 import org.dsa.iot.dslink.node.Node;
 import org.dsa.iot.dslink.node.NodeBuilder;
-import org.dsa.iot.dslink.util.Objects;
 import org.dsa.iot.onem2m.actions.cse.AddCSE;
 import org.dsa.iot.onem2m.actions.server.DeleteServer;
 import org.dsa.iot.onem2m.actions.server.EditServer;
 import org.opendaylight.iotdm.client.Exchange;
 import org.opendaylight.iotdm.primitive.RequestPrimitive;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
 /**
  * @author Samuel Grenier
  */
 public class OneM2MServer {
 
-    private List<String> paths = new ArrayList<>();
     private final Node parent;
 
     private OneM2MServer(Node parent) {
@@ -27,24 +22,6 @@ public class OneM2MServer {
             throw new NullPointerException("parent");
         }
         this.parent = parent;
-        setupPolling();
-    }
-
-    private void setupPolling() {
-        Objects.getDaemonThreadPool().scheduleWithFixedDelay(new Runnable() {
-            @Override
-            public void run() {
-                // pollling logic for onem2m
-            }
-        }, 5, 5, TimeUnit.SECONDS);
-    }
-
-    public void subscribe(String path) {
-        paths.add(path);
-    }
-
-    public void unsubcribe(String path) {
-        paths.remove(path);
     }
 
     public void addCSE(String cse) {
